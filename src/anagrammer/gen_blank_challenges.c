175bfeb460e0db8d8598dbbbb520e60311c5a648
Cesar Del Solar <delsolar@gmail.com>
2013-05-21 01:13:08 -0700

    Write blank challenges to a file.

--- src/anagrammer/gen_blank_challenges.c
+++ src/anagrammer/gen_blank_challenges.c
@@ -7,21 +7,26 @@
 #include <time.h>
 
 struct Answers answers;
-void gen_blank_challenges(NODE* node, int word_length, int num, int max_sol);
+void gen_blank_challenges(NODE* node, int word_length, int num, int max_sol,
+                          FILE* fp);
 GRand* g_rand;
 char letter_distribution[101] =
     "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ??\0";
 
 int main(int argc, char **argv) {
-    if (argc != 5) {
-        printf("Arguments: gaddag_path num_questions max_ans_per_q length\n");
+    if (argc != 6) {
+        printf("Arguments: gaddag_path num_questions max_ans_per_q length");
+        printf(" filename\n");
         return 1;
     }
     g_rand = g_rand_new();
     printf("Loading gaddag...");
     NODE* node = load_gaddag(argv[1]);
-    gen_blank_challenges(node, atoi(argv[4]), atoi(argv[2]), atoi(argv[3]));
+    FILE* fp = fopen(argv[5], "w");
+    gen_blank_challenges(node, atoi(argv[4]), atoi(argv[2]), atoi(argv[3]),
+                         fp);
     g_rand_free(g_rand);
+    fclose(fp);
     return 0;
 }
 
@@ -44,20 +49,24 @@ void gen_random_rack(char* str, int num_letters, int num_blanks) {
  * @return  1 if success, 0 if failure
  */
 int try_generate_blank_challenge(int word_length, int max_sol, int num_blanks,
-                             NODE* node) {
+                             NODE* node, FILE* fp) {
     char str[16];
-    int success;
+    int success, i;
     gen_random_rack(str, word_length - num_blanks, num_blanks);
     success = anagram(node, str, "anagram", &answers, 0, max_sol);
     if (success) {
-        printf("%s ", str);
-        print_answers(&answers);
+        fprintf(fp, "%s ", str);
+        for (i = 0; i < answers.num_answers; i++) {
+            fprintf(fp, "%s ", answers.answers[i]);
+        }
+        fprintf(fp, "\n");
         cleanup_answers(&answers);
     }
     return success;
 }
 
-void gen_blank_challenges(NODE* node, int word_length, int num, int max_sol) {
+void gen_blank_challenges(NODE* node, int word_length, int num, int max_sol,
+                          FILE* fp) {
     uint8_t numgen, success, num_blanks;
     int num_tries;
     numgen = 0;
@@ -65,7 +74,7 @@ void gen_blank_challenges(NODE* node, int word_length, int num, int max_sol) {
     num_blanks = 1;
     while (1) {
         success = try_generate_blank_challenge(word_length, max_sol, num_blanks,
-                                               node);
+                                               node, fp);
         numgen += success;
         num_tries += 1;
         if (numgen == num - 2) {
